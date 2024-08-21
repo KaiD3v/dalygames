3934264bbf64f121a40a1ec67bb39a55eb3e3370
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-21 14:53:18 -0300

    generate dynamic SEO with metadatas

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -4,24 +4,66 @@ import Image from "next/image";
 import { Container } from "../../../components/container";
 import { Label } from "./components/label";
 import { GameCard } from "../../../components/GameCard";
+import { Metadata } from "next";
 
+interface PropsParams {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({
+  params
+}: PropsParams): Promise<Metadata> {
+  try {
+    const response: GameProps = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`,
+      { cache: "no-store" }
+    ).then((res) => res.json());
+
+    return {
+      title: response.title,
+      description: `${response.description.slice(0, 100)}...`,
+      openGraph: {
+        title: response.title,
+        images: [response.image_url]
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true
+        }
+      }
+    };
+  } catch (error) {
+    return {
+      title: "DalyGames - Descubra jogos incríveis e se divirta!"
+    };
+  }
+}
 
 async function getDalyGame() {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_API_URL}/next-api/?api=game_day`, {cache: "no-store"}
-      );
-  
-      return res.json();
-    } catch (error) {
-      throw new Error("Failed to fetch data.");
-    }
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { cache: "no-store" }
+    );
+
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data.");
   }
+}
 
 async function getData(id: string) {
   try {
     const res = await fetch(
-      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, {cache: "no-store"}
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`,
+      { cache: "no-store" }
     );
 
     return res.json();
@@ -36,7 +78,7 @@ export default async function Game({
   params: { id: string };
 }) {
   const data: GameProps = await getData(id);
-  const gameDay: GameProps = await getDalyGame()
+  const gameDay: GameProps = await getDalyGame();
 
   if (!data) {
     redirect("/");
@@ -56,34 +98,32 @@ export default async function Game({
         />
       </div>
       <Container>
-        <h1 className="font-bold text-xl my-4">
-          {data.title}
-        </h1>
-        <p>
-          {data.description}
-        </p>
+        <h1 className="font-bold text-xl my-4">{data.title}</h1>
+        <p>{data.description}</p>
         <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
         <div className="flex gap-2 flex-wrap">
-            {data.platforms.map(item => (
-                <>
-                    <Label name={item} key={item}/>
-                </>
-            ))}
+          {data.platforms.map((item) => (
+            <>
+              <Label name={item} key={item} />
+            </>
+          ))}
         </div>
         <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
         <div className="flex gap-2 flex-wrap">
-            {data.categories.map(item => (
-                <>
-                    <Label name={item} key={item}/>
-                </>
-            ))}
+          {data.categories.map((item) => (
+            <>
+              <Label name={item} key={item} />
+            </>
+          ))}
         </div>
-        <p><strong>Data de lançamento:</strong> {data.release}</p>
+        <p>
+          <strong>Data de lançamento:</strong> {data.release}
+        </p>
         <h2 className="font-bold text-lg mt-7 mb-2">Jogo Recomendado:</h2>
         <div className="flex">
-            <div className="flex-grow">
+          <div className="flex-grow">
             <GameCard data={gameDay} />
-            </div>
+          </div>
         </div>
       </Container>
     </main>
