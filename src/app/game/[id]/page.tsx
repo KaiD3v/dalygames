0ef5cd1a5e2cff4be93db26f68a287385b5fdb5a
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-20 22:27:59 -0300

    add details page pt.02 - Page strucuture and Label component

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -2,11 +2,26 @@ import { redirect } from "next/navigation";
 import { GameProps } from "../../../utils/types/game";
 import Image from "next/image";
 import { Container } from "../../../components/container";
+import { Label } from "./components/label";
+import { GameCard } from "../../../components/GameCard";
+
+
+async function getDalyGame() {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_API_URL}/next-api/?api=game_day`, {cache: "no-store"}
+      );
+  
+      return res.json();
+    } catch (error) {
+      throw new Error("Failed to fetch data.");
+    }
+  }
 
 async function getData(id: string) {
   try {
     const res = await fetch(
-      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, {cache: "no-store"}
     );
 
     return res.json();
@@ -21,6 +36,7 @@ export default async function Game({
   params: { id: string };
 }) {
   const data: GameProps = await getData(id);
+  const gameDay: GameProps = await getDalyGame()
 
   if (!data) {
     redirect("/");
@@ -28,7 +44,48 @@ export default async function Game({
 
   return (
     <main className="w-full text-black">
-
+      <div className="bg-black h-80 sm:h-96 w-full relative">
+        <Image
+          className="object-cover w-full h-80 sm:h-96 opacity-80"
+          src={data.image_url}
+          alt={data.title}
+          priority={true}
+          fill={true}
+          quality={100}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+        />
+      </div>
+      <Container>
+        <h1 className="font-bold text-xl my-4">
+          {data.title}
+        </h1>
+        <p>
+          {data.description}
+        </p>
+        <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+        <div className="flex gap-2 flex-wrap">
+            {data.platforms.map(item => (
+                <>
+                    <Label name={item} key={item}/>
+                </>
+            ))}
+        </div>
+        <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+        <div className="flex gap-2 flex-wrap">
+            {data.categories.map(item => (
+                <>
+                    <Label name={item} key={item}/>
+                </>
+            ))}
+        </div>
+        <p><strong>Data de lançamento:</strong> {data.release}</p>
+        <h2 className="font-bold text-lg mt-7 mb-2">Jogo Recomendado:</h2>
+        <div className="flex">
+            <div className="flex-grow">
+            <GameCard data={gameDay} />
+            </div>
+        </div>
+      </Container>
     </main>
   );
 }
